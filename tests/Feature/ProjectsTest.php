1d05a1bee2d7495aa448b4b0d555bc958752dd53
david-vargas98 <edgar981125@live.com.mx>
2024-01-11 11:12:32 -0600

    feat: Testing
    Se implementa el métodom para las pruebas en la base de datos

--- tests/Feature/ProjectsTest.php
+++ tests/Feature/ProjectsTest.php
@@ -8,13 +8,26 @@ use Tests\TestCase;
 
 class ProjectsTest extends TestCase
 {
+    use WithFaker; //This is useful because we need to make fake data in the attributes array
+    use RefreshDatabase; //This is useful because after the test this ensures we reset everything back to the initial state
     /**
      * A basic feature test example.
+     * @return void
      */
-    public function test_example(): void
+    /** @test */ 
+    public function a_user_can_create_a_project(): void
     {
-        $response = $this->get('/');
+        //An arrray of attributes using faker
+        $attributes = [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph
+        ];
+        //By making a post request, a project will be created
+        $this->post('/projects', $attributes);
 
-        $response->assertStatus(200);
+        //This makes it to be seen on the database table
+
+        //After I make the request, i'm going to assert that the database has a projects table that include the previous attributes
+        $this->assertDatabaseHas('projects', $attributes);
     }
 }
