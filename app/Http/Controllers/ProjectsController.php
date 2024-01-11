5a3f58677e0a34be4a18bf96a6460f771e67f177
david-vargas98 <edgar981125@live.com.mx>
2024-01-11 13:27:46 -0600

    refactor: Rutas
    Se pasa la lógica del index y store a el controlador, originalmente estaban declarados en el archivo web de rutas

--- app/Http/Controllers/ProjectsController.php
+++ app/Http/Controllers/ProjectsController.php
@@ -3,8 +3,23 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
-
+use App\Models\Project;
 class ProjectsController extends Controller
 {
-    //
+    public function index()
+    {
+        //We want to fetch all of the projects
+        $projects = Project::all();
+
+        return view('projects.index', compact('projects'));
+    }
+
+    public function store()
+    {
+        //Validation
+
+        //Perpetuation
+        Project::create(request(['title', 'description']));
+        //Redirection
+    }
 }
